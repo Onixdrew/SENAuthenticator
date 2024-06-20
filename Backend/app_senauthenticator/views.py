f97b187a219dad50e588a289de45236e655661b3
juls2404 <juliethximenarendonsanchez@gmail.com>
2024-06-20 15:32:47 -0500

    Autenticacion 2

--- Backend/app_senauthenticator/views.py
+++ Backend/app_senauthenticator/views.py
@@ -17,7 +17,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import authentication_classes
 from rest_framework. permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
@@ -90,3 +90,47 @@ class IngresoListarCrear(generics.ListCreateAPIView):
 class IngresoDetalles(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ingreso.objects.all()
     serializer_class = IngresoSerializer
+    
+
+@api_view(['POST'])
+def login(request):
+    
+    user = get_object_or_404(Usuario, nombre_usuario= request.data['nombre_usuario'])
+    
+    if not user.check_password(request.data['password']):
+        return Response({"error": "Invalid Password"}, status = status.HTTP_404_NOT_FOUND)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer= UserSerializer(instance=user)
+    
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK) 
+
+
+@api_view(['POST'])
+def register(request):
+    print(request.data)
+    
+    serializer = UserSerializer(data= request.data)
+    
+    
+    if serializer.is_valid():
+        serializer.save()
+        
+        user= User.objects.get(username= serializer.data["username"])
+        user.set_password(serializer.data['password'])
+        user.save()
+        token = Token.objects.create(user= user)
+        return Response ({'token': token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    
+    print (request.user.id) 
+    serializer = UserSerializer(instance=request.user)
+    
+    return Response(serializer.data, status=status.HTTP_200_OK)
