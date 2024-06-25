3d5c3eed547173cfdc296796784d90890308939c
Juan Ochoa <loljuanchogaymer@gmail.com>
2024-06-25 04:03:34 -0500

    Solucion de errores Front

--- Movil/lib/ui/pages/login/login_screen.dart
+++ Movil/lib/ui/pages/login/login_screen.dart
@@ -4,6 +4,8 @@ import 'package:reconocimiento_app/ui/pages/home/home_page.dart';
 import 'package:reconocimiento_app/ui/pages/register/register_screen.dart';
 
 class Login extends StatefulWidget {
+  const Login({super.key});
+
   @override
   State<Login> createState() => _LoginState();
 }
@@ -72,158 +74,162 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     // Construir la interfaz de usuario
-    return Scaffold(
-      backgroundColor: Colors.green,
-      body: Stack(
-        children: [
-          // Logo en la esquina superior izquierda
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-                'images/login/LogoReconocimientoFacialBlanco.png',
-                height: 50),
-          ),
-          // Logo en la esquina superior derecha
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Image.asset('images/login/logoSenaBlanco.png', height: 50),
-          ),
-
-          // Contenedor blanco con bordes redondeados
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20)),
-            margin: const EdgeInsets.only(
-                top: 100, left: 20, right: 20, bottom: 50),
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Center(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Título "Ingreso Usuario Registrado"
-                    const Text("Ingreso Usuario Registrado"),
-                    const SizedBox(height: 20),
-                    // Selección de tipo de documento
-                    const Text("Tipo de documento"),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: DropdownButton<String>(
-                        isExpanded: true,
-                        value: selectedDocumentType,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedDocumentType = newValue!;
-                          });
-                        },
-                        items: documentTypes
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        underline: const SizedBox(), // Remove the underline
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    // Campo de texto para número de documento
-                    const Text("Número de documento"),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextFormField(
-                        controller: documentNumberController,
-                        decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Número de documento"),
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor ingrese su número de documento';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    // Campo de texto para contraseña
-                    const Text("Contraseña"),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextFormField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                            border: InputBorder.none, hintText: "Contraseña"),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor ingrese su contraseña';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    // Botón "Iniciar sesión"
-                    Container(
-                      width: 200,
-                      decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: TextButton(
-                        onPressed: _login,
-                        child: const Text(
-                          "Iniciar sesión",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.green,
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              // Logo en la esquina superior izquierda
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset(
+                    'images/login/LogoReconocimientoFacialBlanco.png',
+                    height: 50),
+              ),
+              // Logo en la esquina superior derecha
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Image.asset('images/login/logoSenaBlanco.png', height: 50),
+              ),
+                
+              // Contenedor blanco con bordes redondeados
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                margin: const EdgeInsets.only(
+                    top: 100, left: 20, right: 20, bottom: 50),
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Center(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Título "Ingreso Usuario Registrado"
+                        const Text("Ingreso Usuario Registrado"),
+                        const SizedBox(height: 20),
+                        // Selección de tipo de documento
+                        const Text("Tipo de documento"),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: DropdownButton<String>(
+                            isExpanded: true,
+                            value: selectedDocumentType,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedDocumentType = newValue!;
+                              });
+                            },
+                            items: documentTypes
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            underline: const SizedBox(), // Remove the underline
+                          ),
                         ),
-                      ),
+                        const SizedBox(height: 20),
+                        // Campo de texto para número de documento
+                        const Text("Número de documento"),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextFormField(
+                            controller: documentNumberController,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Número de documento"),
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor ingrese su número de documento';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        // Campo de texto para contraseña
+                        const Text("Contraseña"),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none, hintText: "Contraseña"),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor ingrese su contraseña';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        // Botón "Iniciar sesión"
+                        Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: TextButton(
+                            onPressed: _login,
+                            child: const Text(
+                              "Iniciar sesión",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        // Enlace "¿Olvidaste tu contraseña?"
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "¿Olvidaste tu contraseña?",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        // Enlace "¿Deseas registrarte?"
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const RegisterScreen()));
+                          },
+                          child: const Text(
+                            "¿Deseas registrarte?",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 20),
-                    // Enlace "¿Olvidaste tu contraseña?"
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "¿Olvidaste tu contraseña?",
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    // Enlace "¿Deseas registrarte?"
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegisterScreen()));
-                      },
-                      child: const Text(
-                        "¿Deseas registrarte?",
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
