9d9eb10b44db2af503feb97cd402bcc09ce540a4
daniel-astaiza <danielastaiza111@gmail.com>
2024-06-25 22:01:05 -0500

    nueva actualizacion

--- front/src/app/Administrador/AdministradorHome/page.jsx
+++ front/src/app/Administrador/AdministradorHome/page.jsx
@@ -1,45 +1,41 @@
-import React from 'react'
-import Navbar from '@/components/Navbar/navbar'
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import Navbar from '@/components/Navbar/navbar';
 import Image from 'next/image';
 
-import imagen from "../../../../public/assets/niño.jpg"
+import imagen1 from "../../../../public/assets/foto1.jpg";
+import imagen2 from "../../../../public/assets/foto2.jpg";
+import imagen3 from "../../../../public/assets/foto3.jpg";
 
-export default function page() {
+export default function Page() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  
+  const slides = [imagen1, imagen2, imagen3];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [slides]);
+
   return (
     <div>
-    <header>
-      <Navbar op1="home" op2='reportes' op3='solicitudes' />
-    </header>
-    <h1>Hola</h1>
-    <div className="flex items-center justify-center">
-      <div className="flex justify-center space-x-4 mt-[30vh]">
-        <div className="w-32 h-32">
-          <Image
-            src={imagen}
-            width={200}
-            height={200}
-            alt="Imagen 1"
-          />
-        </div>
-        <div className="w-32 h-32">
-          <Image
-            src={imagen}
-            width={200}
-            height={200}
-            alt="Imagen 2"
-          />
-        </div>
-        <div className="w-32 h-32">
-          <Image
-            src={imagen}
-            width={200}
-            height={200}
-            alt="Imagen 3"
-          />
-        </div>
+      <header>
+        <Navbar op1="Home" op2='Reportes' op3='Solicitudes' />
+      </header>
+      <h1>Hola</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a rerum ipsa cupiditate numquam quaerat quod aliquam nulla, voluptatibus sequi ut minima voluptates ea. Rem iusto perferendis esse expedita ipsum?</p>
+      <div className="carousel w-full flex justify-center items-center">
+        {slides.map((slide, index) => (
+          <div key={index} className={`carousel-item relative w-full ${index === currentSlide ? 'block' : 'hidden'}`}>
+            <Image src={slide} alt={`Slide Image ${index + 1}`} width={400} height={400} className="rounded-lg mx-auto" />
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-  )
+  );
 }
-
