46e446fa665e0c719a875dd3df59f823959b39fa
Onixdrews <andrewmosquera1811@gmail.com>
2024-07-29 17:06:33 -0500

    'Updated'

---
+++ front/src/pages/Instructor/inicioIntructor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const InicioIntructor = () => {
+  return (
+    <>
+    <div>
+        <Navbar></Navbar>
+    </div>
+    </>
+  )
+}
+
+export default InicioIntructor
